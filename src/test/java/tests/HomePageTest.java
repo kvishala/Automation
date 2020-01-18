68ca393ef54a49ac429afb4bef5af508a9750a25
kvishala <kvishala@gmail.com>
2020-01-18 22:04:55 +0530

    yoga

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -14,7 +14,7 @@ public class HomePageTest {
 	}
 
 	@Test
-	public void verifyWomenTab1() {
+	public void verifyWomenTab123() {
 		Assert.assertTrue(homePage.elementFound(homePage.getLnkTxtWomen()), "Failed: women tab not found");
 	}
 
